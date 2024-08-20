d53618705492719278019a541c517c2163ab9847
s5unnyjjj <s5unnyjjj@gmail.com>
2024-08-20 20:44:59 +0900

    Programmers_240820

---
+++ 02_Programmers/028_Lv1_이웃한칸.py
@@ -0,0 +1,16 @@
+
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/250125
+
+dh = [0, 1, -1, 0]
+dw = [1, 0, 0, -1]
+
+def solution(board, h, w):
+    answer = 0
+    n = len(board)
+    for i in range(4):
+        h_check = h + dh[i]
+        w_check = w + dw[i]
+        if 0 <= h_check < n and 0 <= w_check < n:
+            if board[h][w] == board[h_check][w_check]:
+                answer += 1
+    return answer
